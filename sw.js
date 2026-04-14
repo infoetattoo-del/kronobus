50d78cecd9bfbff8da9479e47e3dfd7d153ef25a
infoetattoo-del <infoetattoo@gmail.com>
2026-04-14 22:13:49 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+var CACHE = 'kronobus-v1';
+var FILES = ['./index.html', './'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) {
+      return c.addAll(FILES);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      if (cached) return cached;
+      return fetch(e.request).then(function(resp) {
+        return caches.open(CACHE).then(function(c) {
+          c.put(e.request, resp.clone());
+          return resp;
+        });
+      }).catch(function() {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
